b2a8fc9cd5fd21c8b363ba25c8c4716359bb9aeb
Candra Andika Putra <88470704+Candrandika@users.noreply.github.com>
2024-10-08 08:39:05 +0700

    mod: ignoring yarn lock file

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,20 @@
+
+import { Outlet } from 'react-router-dom'
+import { Header } from './parts-main/Header'
+import { Sidebar } from './parts-main/Sidebar'
+
+export const MainLayout = () => {
+    return (
+        <>
+        <Sidebar />
+            <div className="flex w-full items-stretch ps-0 md:ps-64 bg-background">
+                <div className="flex-1">
+                    <Header/>
+                    <div className='p-6'>
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
