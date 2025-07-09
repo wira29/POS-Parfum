d09899d452f2ce161ce5ed5be3f8300563f702d1
Udean23 <ahmadsyarifudinnajihulw@gmail.com>
2025-07-09 19:36:43 +0700

    update

--- src/views/pages/products/index.tsx
+++ src/views/pages/products/index.tsx
@@ -535,13 +535,6 @@ export const ProductIndex = () => {
                     >
                       Import
                     </button>
-                    <button
-                      className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed"
-                      onClick={handleImport}
-                    //disabled={!importFile && !importUrl}
-                    >
-                      Eksport
-                    </button>
                   </div>
                 </div>
               </div>
